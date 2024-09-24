c724f67e08d02a42e8b725eb98e6c06bfe759494
Harry Lawal <harrylawal74@gmail.com>
2024-09-24 15:55:39 +0100

    initial caesa cipher

---
+++ python_functions.py
@@ -0,0 +1,10 @@
+text = 'Hello World'
+shift = 3
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#outputs the datatype if variable 'alphabet'
+print(type(alphabet))
+
+#searches variable for specified data
+alphabet.find("z")
